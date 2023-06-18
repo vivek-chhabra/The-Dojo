1f30883aaf2e7ff0ec63260680fb54af782012a4
vivek chhabra <vivekcb8642@gmail.com>
2023-06-18 14:15:46 +0530

    add routes in the app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import ProjectDetails from "./pages/project/ProjectDetails";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import Home from "./pages/home(dashboard)/Home";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
+import Create from "./pages/create/Create";
+import NavBar from "./components/NavBar";
+import Login from "./pages/login/Login";
 import "./App.css";
 
 function App() {
+    // using params
+    const { id } = useParams();
+    // console.log(id)
     return (
-        <div className="App">
-         
-        </div>
+        <>
+            <NavBar />
+            <Routes>
+                <Route path="/The-Dojo" element={<Navigate to={"/"} />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/create" element={<Create />} />
+                <Route path="/project" element={<Project />} />
+                <Route path="/project/:id" element={<ProjectDetails />} />
+            </Routes>
+        </>
     );
 }
 
